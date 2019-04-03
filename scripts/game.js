079957da19f7caba4dca29ef9d0c29eddcb1c6c0
Alina Ghetler <alina.ghetler@gmail.com>
2019-04-03 16:06:03 +0200

    creating the start button and implement it

--- scripts/game.js
+++ scripts/game.js
@@ -3,6 +3,8 @@ let gif_createImg1, gif_createImg2, gif_createImg3, gif_createImg4;
 let bg;
 let basket;
 let counter = 0;
+let button;
+let start;
 
 function setup() {
     // preload grafical elements
@@ -15,6 +17,10 @@ function setup() {
     bg = loadImage("../asset/farm.jpg");
     createCanvas(1000, 730);
     // Create ball object(type, points, startX,startY,Size)
+    button = createButton('start Game')
+    button.mousePressed(startGame)
+    button.position(900, 700);
+    button =background(0);
     egg = new Egg("normal", 10, 130, 100, 20, 30);
     basket = new Basket(500, 530, 200, 200);
     basket.setup();
@@ -26,14 +32,18 @@ function draw() {
     gif_createImg2.position(280, 30);
     gif_createImg3.position(500, 30);
     gif_createImg4.position(700, 30);
-    egg.display();
-    basket.display();
+    
     let hit = collideRectCircle(basket.coordX, basket.coordY, basket.width, basket.height, egg.x, egg.y, egg.w);
     if (hit) {
         counter += 1;
     }
     egg.update(hit);
-
+    if(start) {
+     egg.display() 
+     basket.display()
+    } else {
+    
+    }
     textSize(32); 
     fill(0, 102, 153);
     text(counter, 950, 50);
@@ -46,4 +56,8 @@ function keyPressed() {
     if (keyCode === RIGHT_ARROW) {
         basket.moveRight(50);
     }
+}
+
+function startGame(){
+   start = true 
 }
